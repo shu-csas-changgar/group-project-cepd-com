5c51811e7342150188ddbbfa4a319d7225c26d2c
Emmanuel Olatunde <olatundemma006@gmail.com>
2021-10-06 16:31:28 -0400

    Class Updates

--- ABC_CORP/ABC_CORP_APP/views.py
+++ ABC_CORP/ABC_CORP_APP/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    data = {
+        'name': 'Garett Chang',
+        'email': 'changgar@shu.edu',
+        'message': 'Hello from CSAS 4117'
+    }
+    return render(request, 'Index.html', context=data)
