32defff9587ff6d776c354c2a298c8000534cd54
Emmanuel Olatunde <olatundemma006@gmail.com>
2021-10-18 16:42:15 -0400

    Final Updates

--- ABC_CORP/registration/views.py
+++ ABC_CORP/registration/views.py
@@ -1,7 +1,5 @@
-from django.http.response import Http404
 from django.shortcuts import render
-from django.http import Http404
 
-
+# Create your views here.
 def login(request):
     return render(request, 'login.html', {})
