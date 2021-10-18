f90650784cc6e616c010ab0a3e283278f9471b05
Emmanuel Olatunde <olatundemma006@gmail.com>
2021-10-18 16:30:38 -0400

    Updated Registration App name

--- ABC_CORP/ABC_CORP/urls.py
+++ ABC_CORP/ABC_CORP/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import RedirectView
 
-from registration import views
+from Registration import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
