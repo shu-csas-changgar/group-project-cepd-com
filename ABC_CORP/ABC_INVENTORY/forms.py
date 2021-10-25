f061c47b5321b9644ac55c8d749a6fb9ff0663c8
Charles Edward Baiden <baidenskyress@gmail.com>
2021-10-25 16:47:39 -0400

    99.99 percent done

---
+++ ABC_CORP/ABC_INVENTORY/forms.py
@@ -0,0 +1,7 @@
+from django.contrib.auth.forms import UserCreationForm
+from .models import User
+
+class CreateUserForm(UserCreationForm):
+	class Meta:
+		model = User
+		fields = ['firstName', 'lastName', 'email', 'phone','address','officeLocation','password1', 'password2']
--- ABC_CORP/ABC_INVENTORY/views.py
+++ ABC_CORP/ABC_INVENTORY/views.py
@@ -1,10 +1,62 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+from .forms import CreateUserForm
+from django.contrib.auth.models import Group
+from .models import  User, Location
 
 def loginPage(request):
+    if request.method == 'POST':
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+
+        user = authenticate(request, email=email, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.info(request, 'Email OR Password is incorrect')
     return render(request, 'login.html', {})
 
+
 def registerPage(request):
-    return render(request, 'register.html', {})
+
+    form = CreateUserForm()
+    if request.method == 'POST':
+
+        form = CreateUserForm(request.POST)
+
+
+
+        #firstN = request.POST.get('first-name')
+        #lastN = request.POST.get('last-name')
+        #email = request.POST.get('email')
+        #phone = request.POST.get('phone')
+        #password1 = request.POST.get('password1')
+        #password2 = request.POST.get('password2')
+        #address = request.POST.get('address')
+        #location=request.POST.get('location')
+        #office=Location(name=location)
+        #office.save()
+
+        #user = User.objects.create_user(firstName=firstN, lastName=lastN,
+        #                        email=email, phone=phone, address=address,
+        #                        officeLocation=office,
+        #                        password=password1)
+
+        #form = CreateUserForm(request.POST)
+        #print("YEs")
+        #print("Yes")
+        if form.is_valid():
+            user = form.save()
+            name = form.cleaned_data.get('firstName')
+            messages.success(request, 'Account was created for ' + firstName + " " + lastName)
+            return redirect('login')
+
+
+
+    return render(request, 'register.html', {'form':form})
 
 def logoutUser(request):
     pass
